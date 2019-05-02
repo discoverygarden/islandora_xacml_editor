515c917ed72eb06566570c304f298f0ce74f3dc7
willtp87 <willtp87@hotmail.com>
2019-05-02 09:50:36 -0300

    Some doc improvements.

--- src/Commands/IslandoraXacmlEditorCommands.php
+++ src/Commands/IslandoraXacmlEditorCommands.php
@@ -7,15 +7,7 @@ use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\islandora_xacml_editor\IslandoraUpdatePolicy;
 
 /**
- * A Drush commandfile.
- *
- * In addition to this file, you need a drush.services.yml
- * in root of your module, and a composer.json file that provides the name
- * of the services file to use.
- *
- * See these files for an example of injecting Drupal services:
- *   - http://cgit.drupalcode.org/devel/tree/src/Commands/DevelCommands.php
- *   - http://cgit.drupalcode.org/devel/tree/drush.services.yml
+ * Drush commandfile for Islandora XACML Editor.
  */
 class IslandoraXacmlEditorCommands extends DrushCommands {
 
